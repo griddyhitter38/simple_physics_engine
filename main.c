11ceb4335be6ec51efafddad563b0f18891e895c
griddyhitter38 <rdronjak38@gmail.com>
2025-12-28 17:44:15 +0200

    update

--- main.c
+++ main.c
@@ -42,32 +42,50 @@ void drawKirkle(SDL_Renderer *renderer, int cx, int cy, int radius, int mass, fl
     }
 }
 
-int main(){
+int main(void){
     bool running = true;
 
     SDL_Window *window = NULL;
     SDL_Renderer *renderer = NULL;
     SDL_Event event;
 
-    SDL_Init(SDL_INIT_VIDEO);
-    SDL_CreateWindowAndRenderer(WIDTH, HEIGHT,  SDL_WINDOW_SHOWN, &window, &renderer);
+    if (SDL_Init(SDL_INIT_VIDEO) != 0) {
+            SDL_Log("init error: %s", SDL_GetError());
+            return 1;
+        }
 
-    //kirle white
-    int r = 255;
-    int green = 255;
-    int b = 255;
-    int a = 255;
+    if (SDL_CreateWindowAndRenderer(WIDTH, HEIGHT, SDL_WINDOW_SHOWN, &window, &renderer) != 0) {
+           SDL_Log("createwindow error: %s", SDL_GetError());
+           SDL_Quit();
+           return 1;
+       }
+
+    //kirle apperance
+    const Uint8 R = 255,G = 255,B = 255,A = 255;
     int radius = 70;
 
-    //physics
-    float x = WIDTH / 2;
-    float y = HEIGHT / 2;
+    //physics state
+    float x = WIDTH / 2.0f;
+    float y = HEIGHT / 2.0f;
     float vx = 0.0f;
     float vy = 0.0f;
 
-    const float control = 20000.0f;
+    //acceleration
+    float ax = 0.0f;
+    float ay = 0.0f;
+
+    //drag
     const float drag = 0.98f;
-    const float g = 10000.0f;
+
+    //control accel
+    const float inputAccel = 20000.0f;
+
+
+    //gravity
+    float y0 = 0.0f;
+    const float g0 = 1000.0f;
+    const float k = 8.0f;
+    const float ayLimit = 40000.0f;
 
     Uint32 lastTime = SDL_GetTicks();
 
@@ -79,40 +97,66 @@ int main(){
             running = false;
 
         }
+
+        //dt
         Uint32 currentTime = SDL_GetTicks();
         float deltaTime = (currentTime - lastTime) / 1000.0f;
         lastTime = currentTime;
-        if (deltaTime > 0.05f)
-            deltaTime = 0.05f;
+        if (deltaTime > 0.05f) deltaTime = 0.05f;
 
         const Uint8 *keys = SDL_GetKeyboardState(NULL);
 
-        if (keys[SDL_SCANCODE_LEFT]) vx -= control * deltaTime;
-        if (keys[SDL_SCANCODE_RIGHT]) vx += control * deltaTime;
-        if (keys[SDL_SCANCODE_UP]) vy -= control * deltaTime;
-        if (keys[SDL_SCANCODE_DOWN]) vy += control * deltaTime;
+        ax = 0.0f;
+        ay = 0.0f;
 
-    while (vy < HEIGHT - radius){
-        vy += g * deltaTime;
-    }
+        if (keys[SDL_SCANCODE_LEFT]) ax -= inputAccel;
+        if (keys[SDL_SCANCODE_RIGHT]) ax += inputAccel;
+        if (keys[SDL_SCANCODE_UP]) ay -= inputAccel;
+        if (keys[SDL_SCANCODE_DOWN]) ay += inputAccel;
+
+        float dy = y - y0;
+        if (dy < 0.0f) dy = 0.0f;
+
+        float gravityAy = g0 + k * dy;
+        if(gravityAy > ayLimit) gravityAy = ayLimit;
+
+        ay += gravityAy;
+
+        //vel -> acc
+        vx += ax * deltaTime;
+        vy += ay * deltaTime;
+
+        // drag on vel
         vx *= drag;
         vy *= drag;
-
+        //vel -> pos
         x += vx * deltaTime;
         y += vy * deltaTime;
 
-        // clamp x
-        if (x < radius) x = radius;
-        if (x > WIDTH - radius) x = WIDTH - radius;
+        // collision wall
+        if (x < radius){
+            x = radius;
+            vx = 0.0f;
+        }
+        if (x > WIDTH - radius){
+            x = WIDTH - radius;
+            vx = 0.0f;
+        }
 
-        // clamp y
-        if (y < radius) y = radius;
-        if (y > HEIGHT - radius) y = HEIGHT - radius;
+        // collision up/down
+        if (y < radius){
+            y = radius;
+            vy = 0.0f;
+        }
+        if (y > HEIGHT - radius){
+            y = HEIGHT - radius;
+            vy = 0.0f;
 
-    SDL_SetRenderDrawColor(renderer, 0,0,0,0);
+        }
+    SDL_SetRenderDrawColor(renderer, 0,0,0,A);
     SDL_RenderClear(renderer);
 
-    SDL_SetRenderDrawColor(renderer, r,green,b,a);
+    SDL_SetRenderDrawColor(renderer, R,G,B,A);
     drawFilledKirkle(renderer, (int)x, (int)y, radius);
     //drawKirkle(renderer, posx1, posy1, 100, 10, 0.1);
 
